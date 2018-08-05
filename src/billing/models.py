ac155d1075eece21d2f55cf7b999962036b62122
nikhil-na <nikhilarora1812@gmail.com>
2018-08-05 18:11:44 +0530

    order manager

--- src/billing/models.py
+++ src/billing/models.py
@@ -4,6 +4,22 @@ from django.db.models.signals import post_save
 
 User=settings.AUTH_USER_MODEL
 
+class BillingProfileManager(models.Manager):
+    def new_or_get(self,request):
+        user=request.user
+        guest_email_id = request.session.get('guest_email_id')
+        if user.is_authenticated:
+            # login user checkout ; remembers payment stuff
+            obj, created = BillingProfile.objects.get_or_create(user=user, email=user.email)
+        elif guest_email_id is not None:
+            # guest user checkout ; refreshs payment stuff everytime
+            guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
+            obj, created = BillingProfile.objects.get_or_create(
+                email=guest_email_obj.email)
+        else:
+            pass
+        return obj,created
+
 class BillingProfile(models.Model):
     user=models.OneToOneField(User,null=True,blank=True,on_delete=models.CASCADE)
     email=models.EmailField()
@@ -13,6 +29,8 @@ class BillingProfile(models.Model):
 
     def __str__(self):
         return self.email
+
+    objects=BillingProfileManager()
 
 
 def user_created_reciever(sender,instance,created,*args,**kwargs):
--- src/orders/models.py
+++ src/orders/models.py
@@ -12,6 +12,16 @@ ORDER_STATUS_CHOICES = (
     ('refunded', 'Refunded'),
 )
 
+class OrderManager(models.Manager):
+    def new_or_get(self,billing_profile,cart_obj):
+        qs = self.get_queryset().filter(billing_profile=billing_profile, cart=cart_obj, active=True)
+        if qs.count() == 1:
+            obj = qs.first()
+            created=False
+        else:
+            obj = self.model.objects.create(billing_profile=billing_profile, cart=cart_obj)
+            created=True
+        return obj,created
 
 class Order(models.Model):
     order_id = models.CharField(max_length=120, blank=True)
@@ -25,6 +35,8 @@ class Order(models.Model):
     def __str__(self):
         return self.order_id
 
+    objects=OrderManager()
+
     def update_total(self):
         self.total = math.fsum([self.shipping_total, self.cart.total])
         self.save()
@@ -34,7 +46,9 @@ class Order(models.Model):
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
         instance.order_id = unique_order_id_generator(instance)
-
+    qs=Order.objects.filter(cart=instance.cart).exclude(billing_profile=instance.billing_profile)
+    if qs.exists():
+        qs.update(active=False)
 
 pre_save.connect(pre_save_create_order_id, sender=Order)
 

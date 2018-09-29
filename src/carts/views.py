72c3d6fef3feea8b6d6fd673f78f25bf6adf7955
nikhil-na <nikhilarora1812@gmail.com>
2018-09-29 18:27:49 +0530

    cart updation using AJAX

--- src/carts/views.py
+++ src/carts/views.py
@@ -7,11 +7,20 @@ from billing.models import BillingProfile
 from accounts.models import GuestEmail
 from addresses.forms import AddressForm
 from addresses.models import Address
+from django.http import JsonResponse
 
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
     return render(request, 'carts/home.html', {"cart": cart_obj})
+
+
+def cart_detail_api_view(request):
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    products = [{"name": x.name, "price": x.price, "url": x.get_absolute_url(), "id": x.id, } for x in
+                cart_obj.products.all()]
+    cart_data = {"products": products, "subtotal": cart_obj.subtotal, "total": cart_obj.total}
+    return JsonResponse(cart_data)
 
 
 def cart_update(request):
@@ -20,10 +29,19 @@ def cart_update(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
     if product_obj in cart_obj.products.all():
         cart_obj.products.remove(product_obj)
+        added = False
     else:
         cart_obj.products.add(product_obj)
+        added = True
 
     request.session['cart_items'] = cart_obj.products.count()
+    if request.is_ajax():
+        json_data = {
+            "added": added,
+            "removed": not added,
+            "cartItemCount": cart_obj.products.count(),
+        }
+        return JsonResponse(json_data)
     return redirect('cart:home')
 
 
@@ -38,10 +56,10 @@ def checkout_home(request):
     shipping_address_id = request.session.get('shipping_address_id', None)
     billing_address_id = request.session.get('billing_address_id', None)
     billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
-    address_qs=None
+    address_qs = None
     if billing_profile is not None:
         if request.user.is_authenticated:
-            address_qs=Address.objects.filter(billing_profile=billing_profile)
+            address_qs = Address.objects.filter(billing_profile=billing_profile)
         order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
         if shipping_address_id is not None:
             order_obj.shipping_address = Address.objects.get(id=shipping_address_id)
@@ -52,11 +70,11 @@ def checkout_home(request):
         order_obj.save()
 
     if request.method == 'POST':
-        is_done=order_obj.check_done()
+        is_done = order_obj.check_done()
         if is_done:
             order_obj.mark_paid()
             del request.session['cart_id']
-            request.session['cart_items']=0
+            request.session['cart_items'] = 0
             return redirect('cart:success')
     context = {
         'billing_profile': billing_profile,
@@ -64,10 +82,10 @@ def checkout_home(request):
         'login_form': login_form,
         'guest_form': guest_form,
         'address_form': address_form,
-        'address_qs':address_qs
+        'address_qs': address_qs
     }
     return render(request, 'carts/checkout.html', context)
 
 
 def checkout_done_view(request):
-    return render(request,'carts/checkout-done.html',{})
+    return render(request, 'carts/checkout-done.html', {})
--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth.views import LogoutView
+from carts.views import cart_detail_api_view
 from . import views
 from addresses.views import checkout_address_create_view,checkout_address_reuse_view
 from accounts.views import login_page,register_page,guest_register_view
@@ -18,6 +19,7 @@ urlpatterns = [
     path('register/guest', guest_register_view,name='guest_register'),
     path('logout/', LogoutView.as_view(),name='logout'),
     path('register/', register_page,name='register'),
+    path('cart/api/cart/', cart_detail_api_view,name='api-cart'),
     path('contact/', views.contact,name='contact'),
     path('checkout/address/create',checkout_address_create_view,name='checkout_address_create'),
     path('checkout/address/reuse',checkout_address_reuse_view,name='checkout_address_reuse'),
